dfb728fd47b32a54974a0e8d0fecbee2c3dbfd78
laila22haz <hazmirlaila316@gmail.com>
2024-01-22 02:48:45 +0100

    Here we googit add Implement_Trie.py

---
+++ Implement_Trie.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+class Trie(object):
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word):
+        current = self.root
+        for char in word:
+            if char not in current.children:
+                current.children[char] = Node()
+            current = current.children[char]
+        current.end = True
+
+
+    def search(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.end
+
+
+    def startsWith(self, prefix):
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
